d60c601deb31ef2f362c20c0bf31ca4f44ea6174
pesteban018 <e8293498569@gmail.com>
2024-10-09 10:44:55 -0400

    feat: new funtion, info about me

--- Dart_examples.dart
+++ Dart_examples.dart
@@ -1,11 +1,18 @@
- 
-
- // print hello 10 times
+  // print hello 10 times
 void main() {
   for (int i = 0; i < 10; i++) {
     print('hello ${i + 1}');
   }
 }
+// print info about me
+void main() {
+  String nombre = 'Esteban';
+  int edad = 19;
+  int matricula = 20211076;
+  bool esEstudiante = true;
 
-
-
+  print('Nombre: $nombre');
+  print('Edad: $edad');
+  print('Matricula: $matricula ');
+  print('Es estudiante: $esEstudiante');
+}
